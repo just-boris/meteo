99877e2b8b64fc60a476bf0119db9448a6d88a7c
just-boris <just-boris@hotmail.com>
2014-09-08 01:45:22 +0400

    test(weather): more test cases

--- weather.spec.js
+++ weather.spec.js
@@ -1,11 +1,17 @@
 describe("weather service", function () {
+    function FakeWidget() {
+        this.callback = sinon.spy();
+        this.element = $('<div/>');
+    }
     var weather;
     beforeEach(function () {
         require = makeContext("weather service");
         var spec = this;
         define('city', function() {
             return spec.cityMock = {
-                getCityName: sinon.spy()
+                getCityName: function(callback) {
+                    callback('test-ville');
+                }
             }
         })
     });
@@ -27,12 +33,68 @@ describe("weather service", function () {
         this.server.respondWith("GET", "http://api.openweathermap.org/data/2.5/forecast?&mode=json&units=metric&q=test-ville",
             [200, { "Content-Type": "application/json" },
                 '{"type": "kind a weather data"}']);
-        var callback = sinon.spy(),
-            element = $('<div/>');
-        weather.register(element, callback);
-        this.cityMock.getCityName.firstCall.args[0]('test-ville');
+        var widget = new FakeWidget();
+        weather.register(widget.element, widget.callback);
         this.server.respond();
-        expect(callback).to.have.been.calledOnce;
-        expect(callback).to.have.been.calledWith({"type": "kind a weather data"});
+        expect(widget.callback).to.have.been.calledOnce;
+        expect(widget.callback).to.have.been.calledWith({"type": "kind a weather data"});
+    });
+
+    it("should load weather at once for all widgets", function () {
+        var requestCount = 0;
+        this.server.respondWith(function(xhr) {
+            requestCount++;
+            xhr.respond(200, null, '{"type": "kind a weather data"}');
+        });
+        var widgets = [new FakeWidget(), new FakeWidget()];
+        weather.register(widgets[0].element, widgets[0].callback);
+        weather.register(widgets[1].element, widgets[1].callback);
+        this.server.respond();
+        expect(requestCount).to.equal(1);
+        expect(widgets[0].callback).to.have.been.calledOnce;
+        expect(widgets[1].callback).to.have.been.calledOnce;
+    });
+
+    it("should reload weather after three update events", function () {
+        var requestCount = 0;
+        this.server.respondWith(function(xhr) {
+            requestCount++;
+            xhr.respond(200, null, '{"type": "kind a weather data"}');
+        });
+        var widgets = [new FakeWidget(), new FakeWidget()];
+        weather.register(widgets[0].element, widgets[0].callback);
+        weather.register(widgets[1].element, widgets[1].callback);
+        this.server.respond();
+
+        widgets[0].callback.reset();
+        widgets[1].callback.reset();
+        requestCount = 0;
+
+        widgets[0].element.trigger('update');
+        this.server.respond();
+        expect(requestCount).to.equal(0);
+        expect(widgets[0].callback).not.to.have.been.called;
+
+        widgets[0].element.trigger('update');
+        this.server.respond();
+        expect(requestCount).to.equal(0);
+
+        widgets[1].element.trigger('update');
+        this.server.respond();
+        expect(requestCount).to.equal(0);
+
+        widgets[0].element.trigger('update');
+        this.server.respond();
+        expect(requestCount).to.equal(1);
+        expect(widgets[0].callback).to.have.been.called;
+        expect(widgets[1].callback).to.have.been.called;
+
+        requestCount = 0;
+
+        widgets[0].element.trigger('update');
+        this.server.respond();
+        expect(requestCount).to.equal(0);
+        expect(widgets[0].callback).to.have.been.calledOnce;
+        expect(widgets[1].callback).to.have.been.calledOnce;
     });
 });
